ca441e656b0c6056fc5d84b673fefd135f78e270
fjones36 <fjones36@gmail.com>
2023-03-04 13:09:51 +0000

    updated model.

--- model/yolo.py
+++ model/yolo.py
@@ -8,20 +8,52 @@ class YOLOv1(nn.Module):
         super(YOLOv1, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2),
+            BasicBlock(3, 64, kernel_size=7, stride=2),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=3),
+
+            BasicBlock(64, 192, kernel_size=3),
             nn.MaxPool2d(kernel_size=2, stride=2),
+
             *create_yolo_block(192, [128, 256, 256, 512]),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            
             *create_yolo_block(512, [256, 512] * 4 + [512, 1024]),
             nn.MaxPool2d(kernel_size=2, stride=2),
+
             *create_yolo_block(1024, [512, 1024] * 2),
-            nn.Conv2d(1024, 1024, kernel_size=3),
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=2),
-            nn.Conv2d(1024, 1024, kernel_size=3),
-            nn.Conv2d(1024, 1024, kernel_size=3),
+            BasicBlock(1024, 1024, kernel_size=3),
+            BasicBlock(1024, 1024, kernel_size=3, stride=2),
+            
+            BasicBlock(1024, 1024, kernel_size=3),
+            BasicBlock(1024, 1024, kernel_size=3),
+
+            nn.Flatten(),
+            nn.Dropout(),
+            nn.Linear(7 * 7 * 1024, 4096),
+            nn.BatchNorm1d(4096),
+            nn.LeakyReLU(),
+
+            nn.Linear(4096, 7 * 7 * 30)
         )
+    
+    def forward(self, x):
+        return self.model(x)
+
+class BasicBlock(nn.Module):
+    """
+    Basic YOLO Conv, BatchNorm, LeakyReLU block.
+    """
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1):
+        super(BasicBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU()
+        )
+    
+    def forward(self, x):
+        return self.block(x)
 
 def create_yolo_block(in_channels: int, num_filters: List) -> List[nn.Module]:
     """Create a YOLO block that alternates 1x1 and 3x3 conv layers.
@@ -38,6 +70,6 @@ def create_yolo_block(in_channels: int, num_filters: List) -> List[nn.Module]:
         out_channels = num_filters[i+1]
         kernel_size = 3 if (i + 1) // 2 else 1  # 1 for even layers, 3 for odd
         layers.append(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size)
+            BasicBlock(in_channels, out_channels, kernel_size=kernel_size)
         )
         return layers
